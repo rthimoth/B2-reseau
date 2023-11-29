f7619d5530c4d18cc69fd2e3e2d3f1915a7dfcb6
rthimoth <thimothee.ranvin@hotmail.com>
2023-11-29 23:28:25 +0100

    push partie2

--- tp5/tp5_enc_client2.py
+++ tp5/tp5_enc_client2.py
@@ -1,38 +1,28 @@
 import socket
 import struct
-import re
 
-def send_calculation(sock, num1, operator, num2):
-    # Encodage des entiers et de l'opérateur
-    num1_bytes = struct.pack('!I', num1)  # Format '!I' pour un entier non signé en réseau (big-endian)
-    operator_bytes = struct.pack('!c', operator.encode())  # Format '!c' pour un caractère
-    num2_bytes = struct.pack('!I', num2)
+def send_message(s, number1, operator, number2):
+    # Pack numbers and operator
+    message = struct.pack('>IcI', number1, operator.encode(), number2)
+    # Print the packed message and its length
+    print(f"Packed Message: {message}, Length: {len(message)}")
+    # Send message length followed by the message
+    s.send(struct.pack('>I', len(message)) + message)
 
-    # Envoi de chaque partie avec un en-tête de taille
-    sock.send(num1_bytes)
-    sock.send(operator_bytes)
-    sock.send(num2_bytes)
-
-# Création d'un socket et connexion au serveur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.1.2.12', 13337))
 
-# Récupération et validation de l'entrée utilisateur
-calculation = input("Entrez le calcul (format: 'nombre opérateur nombre'): ")
-match = re.match(r'(\d+)\s*([\+\-\*])\s*(\d+)', calculation)
-while not match:
-    print("Format invalide, veuillez entrer une expression comme '2 + 2'.")
-    calculation = input("Entrez le calcul: ")
-    match = re.match(r'(\d+)\s*([\+\-\*])\s*(\d+)', calculation)
+# User inputs
+number1 = int(input("Enter the first number: "))
+operator = input("Enter the operator (+, -, *): ")
+number2 = int(input("Enter the second number: "))
 
-num1, operator, num2 = match.groups()
+# Send message
+send_message(s, number1, operator, number2)
 
-# Envoi du calcul
-send_calculation(s, int(num1), operator, int(num2))
+# Receive and print the result
+result = s.recv(1024)
+print("Result:", result.decode())
 
-# Réception et affichage du résultat
-result_data = s.recv(1024)
-print(result_data.decode())
-
-# Fermeture de la connexion
 s.close()
+
--- tp5/tp5_enc_server_2.py
+++ tp5/tp5_enc_server_2.py
@@ -1,44 +1,42 @@
 import socket
 import struct
 
-def read_calculation(conn):
-    # Lecture des entiers et de l'opérateur
-    num1_bytes = conn.recv(4)
-    operator_byte = conn.recv(1)
-    num2_bytes = conn.recv(4)
+def process_message(message):
+    number1, operator, number2 = struct.unpack('>IcI', message)
+    operator = operator.decode()
+    if operator == '+':
+        return number1 + number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '*':
+        return number1 * number2
+    else:
+        raise ValueError("Invalid operator")
 
-    # Déballage des entiers et de l'opérateur
-    num1 = struct.unpack('!I', num1_bytes)[0]
-    operator = struct.unpack('!c', operator_byte)[0].decode()
-    num2 = struct.unpack('!I', num2_bytes)[0]
-
-    return num1, operator, num2
-
-# Configuration initiale du serveur
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('10.1.2.12', 13337))
 s.listen(1)
 conn, addr = s.accept()
 
-print("Connecté à:", addr)
+while True:
+    try:
+        # Read message length
+        raw_length = conn.recv(4)
+        if not raw_length:
+            break
+        length = struct.unpack('>I', raw_length)[0]
+        
+        # Read the message
+        message = conn.recv(length)
+        print(f"Received Message: {message}, Length: {length}")
+        result = process_message(message)
 
-try:
-    # Lecture du calcul
-    num1, operator, num2 = read_calculation(conn)
+        # Send the result back
+        conn.send(str(result).encode())
 
-    # Effectuer le calcul
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    else:
-        raise ValueError("Opérateur invalide")
+    except Exception as e:
+        print(f"Error: {e}")
+        break
 
-    # Envoi du résultat
-    conn.send(struct.pack('!I', result))
-finally:
-    conn.close()
-    s.close()
+conn.close()
